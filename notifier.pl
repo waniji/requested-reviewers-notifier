ba7b271d446cedd243a05aae28bd71555fe54e73
Makoto Sasaki <wanigoya@gmail.com>
2018-03-26 15:19:24 +0900

    Refactor again

--- notifier.pl
+++ notifier.pl
@@ -7,68 +7,52 @@ use Encode qw/encode_utf8/;
 use Net::GitHub;
 use WebService::Slack::WebApi;
 
-my $p = Getopt::Long::Parser->new(
-    config => [ "posix_default", "no_ignore_case" ]
-);
+sub main {
+    my $argv = shift;
 
-$p->getoptionsfromarray(\@ARGV, \my %opt, qw/
-    config=s
-    github_token=s
-    github_repos=s
-    slack_token=s
-    slack_channel=s
-/) or die;
-# TODO: use pod2usage
+    my $options = parse_options($argv);
+    my $message = generate_message($options->{github_repos}, $options->{github_token});
+    return unless defined $message;
 
-my $config = {};
-if ($opt{config}) {
-    open (my $fh, '<', $opt{config}) || die "cannot open $!";
-    $config = decode_json do {
-        local $/ = undef;
-        <$fh>;
-    };
+    notify_slack($options->{slack_token}, $options->{slack_channel}, $message);
 }
 
-my @github_repos = do {
-    if ($config->{github_repos}) {
-        @{ $config->{github_repos} };
-    }
-    elsif ($opt{github_repos}) {
-        split(",", $opt{github_repos});
-    }
-};
-my $github_token = $config->{github_token} // $opt{github_token};
-my $slack_token = $config->{slack_token} // $opt{slack_token};
-my $slack_channel = $config->{slack_channel} // $opt{slack_channel};
+sub parse_options {
+    my $argv = shift;
 
-my $github = Net::GitHub->new(access_token => $github_token);
-my $pull_request = $github->pull_request;
-my %review_count;
-my @result;
-for my $github_repo (@github_repos) {
-    my ($account, $repo) = split("/", $github_repo);
-    $pull_request->set_default_user_repo($account, $repo);
-    my @pulls = $pull_request->pulls( { state => 'open' } );
-    for my $pull (@pulls) {
-        my $res = $pull_request->reviewers($pull->{number})->{users};
-        next unless scalar(@$res);
-        my @reviewers = map { sprintf ":%s:", $_->{login} } @$res;
-        $review_count{$_}++ for @reviewers;
-        push @result, generate_pull_request_message($account, $repo, $pull, \@reviewers);
+    my $p = Getopt::Long::Parser->new(
+        config => [ "posix_default", "no_ignore_case" ]
+    );
+
+    $p->getoptionsfromarray($argv, \my %options, qw/
+        config=s
+        github_token=s
+        github_repos=s
+        slack_token=s
+        slack_channel=s
+    /) or die;
+    # TODO: use pod2usage
+
+    my $config = {};
+    if ($options{config}) {
+        open (my $fh, '<', $options{config}) || die "cannot open $!";
+        $config = decode_json do {
+            local $/ = undef;
+            <$fh>;
+        };
     }
+
+    if ($options{github_repos}) {
+        $options{github_repos} = [ split(",", $options{github_repos}) ];
+    }
+
+    $options{github_repos} //= $config->{github_repos};
+    $options{github_token} //= $config->{github_token};
+    $options{slack_token} //= $config->{slack_token};
+    $options{slack_channel} //= $config->{slack_channel};
+
+    return \%options;
 }
-
-exit unless scalar(@result);
-
-push @result, generate_reviewer_count_message(\%review_count);
-
-my $slack = WebService::Slack::WebApi->new(token => $slack_token);
-my $posted_message = $slack->chat->post_message(
-    channel => $slack_channel,
-    username => "Requested Reviewers",
-    text => join("\n", @result),
-    icon_emoji => ":github:",
-);
 
 sub generate_pull_request_message {
     my ($account, $repo, $pull, $reviewers) = @_;
@@ -85,6 +69,47 @@ sub generate_pull_request_message {
 sub generate_reviewer_count_message {
     my $review_count = shift;
 
-    my @sorted = sort { $review_count{$b} <=> $review_count{$a} } keys %$review_count;
-    return join(",", map { sprintf("%s => %d", $_, $review_count{$_}) } @sorted);
+    my @sorted = sort { $review_count->{$b} <=> $review_count->{$a} } keys %$review_count;
+    return join(",", map { sprintf("%s => %d", $_, $review_count->{$_}) } @sorted);
 }
+
+sub generate_message {
+    my ($github_repos, $github_token) = @_;
+
+    my $github = Net::GitHub->new(access_token => $github_token);
+    my $pull_request = $github->pull_request;
+    my %review_count;
+    my @result;
+    for my $github_repo (@$github_repos) {
+        my ($account, $repo) = split("/", $github_repo);
+        $pull_request->set_default_user_repo($account, $repo);
+        my @pulls = $pull_request->pulls( { state => 'open' } );
+        for my $pull (@pulls) {
+            my $res = $pull_request->reviewers($pull->{number})->{users};
+            next unless scalar(@$res);
+            my @reviewers = map { sprintf ":%s:", $_->{login} } @$res;
+            $review_count{$_}++ for @reviewers;
+            push @result, generate_pull_request_message($account, $repo, $pull, \@reviewers);
+        }
+    }
+
+    return unless scalar(@result);
+
+    push @result, generate_reviewer_count_message(\%review_count);
+
+    return join("\n", @result);
+}
+
+sub notify_slack {
+    my ($slack_token, $slack_channel, $message) = @_;
+
+    my $slack = WebService::Slack::WebApi->new(token => $slack_token);
+    my $posted_message = $slack->chat->post_message(
+        channel => $slack_channel,
+        username => "Requested Reviewers",
+        text => $message,
+        icon_emoji => ":github:",
+    );
+}
+
+main(\@ARGV);
